8407f51955445be56f7d38e591a298a4f142232a
Maria Brock <brockm@ornl.giv>
2024-01-26 20:51:56 -0500

    finished module

--- es6-classes.js
+++ es6-classes.js
@@ -173,10 +173,223 @@
 //           Private Members using Symbols
 // *****************************************************
 
-class Circle {
-    constructor(radius) {
-      this._radius = radius;
-    }
+// more abstraction
+// sometimes you'll see an underscore in front of a supposed private property
+// however, this is a code convention but does not actually abstract the property in any way, another developer could still access it and change things
+
+// class Circle {
+//     constructor(radius) {
+//       this._radius = radius;
+//     }
+// }
+
+// const c = new Circle(1);
+
+// we want to hide something from the outside, so if they are changed we want that change limited too within the oject itself
+// no details leaking to the outside. this is not a reccommended approach
+// in ES6 we have a new type, called Symbol(), that we can use to help us abstract
+
+// const _radius = Symbol();
+
+// class Circle {
+//   constructor(radius) {
+//     this._radius = radius;
+//   }
+// }
+
+// const c = new Circle(1);
+
+//this is NOT a constructor function, we can't use new on it
+// if you compare Symbol to another Symbol, they are not ===!
+// this means that each Symbol is given it's own unique value, therefore we can use symbols to replace our property name
+// symbols can be used for property names instead of strings, so these two are functionally the same:
+// this.radius = radius;
+// this[_radius] = radius;
+
+// lets change our constructor and see what happens
+
+// const _radius = Symbol();
+// const _draw = Symbol();
+
+// class Circle {
+//   constructor(radius) {
+//     this[_radius] = radius;
+//   }
+
+//   [_draw]() {
+//     // we can do this with methods too!
+//   }
+// }
+
+// const c = new Circle(1);
+
+//you'll see Symbol() replace the property name, but the property isn't accessible to be changed
+
+
+// *****************************************************
+//           Private Members using WeakMaps
+// *****************************************************
+
+// there's another new type in ES6 called weakMaps
+// these are dictionaries where keys are objects and the value can be anything
+// the keys are weak, if there are no references to them, JS will garbage collect them
+
+// const _radius = new WeakMap();
+// class Circle {
+//   constructor(radius) {
+//     // this.radius = radius;
+//     // we don't declare properties in this instance
+//     // we can call set() on this key map, and then we have to pass an object
+//     // {this} represents the instance of Circle object, add the property reference
+//      _radius.set(this, radius)
+//   }
+// }
+
+// const c = new Circle(1);
+
+// now we don't see anything on the object when we call it
+// but what if we want to call this property somewhere? say in a method within our object?
+
+// const _radius = new WeakMap();
+// const _move = new WeakMap();
+// class Circle {
+//   constructor(radius) {
+
+//      _radius.set(this, radius)
+
+//      _move.set(this, function() {
+//       console.log('move', this);
+//       // this is to show what move means in context
+//      })
+//   }
+
+//   draw() {
+//     // lets say we want to call the private method here in the public method
+//     _move.get(this)()
+//     console.log('draw'); //to help us see what's going on
+//     // console.log(_radius.get(this));
+//   }
+// }
+
+// const c = new Circle(1);
+
+// we can also define private methods such as the _move()
+
+// "this" initially comes back as undefined because classes execute in strict mode
+// but what if we really want to access the instance of Circle here? we can! we have to use an arrow function instead of a regular func
+// arrow funcs use the 'this' value of their containing function
+
+// const _radius = new WeakMap();
+// const _move = new WeakMap();
+// class Circle {
+//   constructor(radius) {
+
+//      _radius.set(this, radius)
+
+//      _move.set(this, () => {
+//       console.log('move', this);
+//       // this will now inherit circleObject and will not rebound
+//      })
+//   }
+
+//   draw() {
+//     _move.get(this)()
+//     console.log('draw'); //to help us see what's going on
+//   }
+// }
+
+// const c = new Circle(1);
+
+// but why are we using separate weakMaps instead of making it on large map?
+// because it puts them all in once large object, which is clunky and a bit polluted
+
+
+// *****************************************************
+//                   Getters & Setters
+// *****************************************************
+
+// const _radius = new WeakMap();
+
+// class Circle {
+//   constructor(radius) {
+//      _radius.set(this, radius)
+//   }
+
+//   // getRadius() {
+//   //   return _radius.get(this)
+//   // }
+
+//   get radius() {
+//     return _radius.get(this);
+//   }
+
+//   set radius(value) {
+//     if (value <= 0) throw new Error('invalid radius');
+//     _radius.set(this, value);
+//   }
+// }
+
+// const c = new Circle(1);
+
+// we have defined a private property, but we want to read it, not change it from the outside
+//we'll add a function getRadius()
+// but to access it, we have to call it like a function, why can't we access it just by the property name?
+// we can read it like a prop with getter and setter functions
+
+// *****************************************************
+//                   Inheritance
+// *****************************************************
+
+//// how do we implement inheritance with ES6?
+// class Shape {
+// constructor(color) {
+//   this.color = color;
+// }
+// // lets add a color to the shapeBase if we need it, and we will add it to our circle
+//   move() {
+//     console.log('move');
+//   }
+// }
+// // the extends syntax!
+// class Circle extends Shape {
+// constructor(color, radius) {
+//   super(color);
+//   //once this is done, you can other properties as needed
+//   this.radius = radius;
+// }
+// // if you add a constructor in the derived class you MUST call super()--the super constructor--to initialize the base object
+//   draw() {
+//     console.log('draw');
+//   }
+// }
+
+// const c = new Circle('dark-cyan', 5);
+
+// we no longer have to manually reassign our object constructor
+
+
+// *****************************************************
+//                   Method Overriding
+// *****************************************************
+
+// remember: this is when we have a method in the objectBase, but we want to implement it differently in our derived object
+
+class Shape {
+  move() {
+    console.log('move');
+  }
 }
 
-const c = new Circle(1);
+class Circle extends Shape {
+  move() {
+    super.move();
+    console.log('circle move');
+  }
+}
+
+const c = new Circle();
+
+// when we inspect the Circle object you will see two move methods: once for Circle, and one for Shape
+// because the JS engine walks up the inheritance tree from the child to the parent, the Circle move() is accessible first
+// what if we want to use some of the original move() functionality though?
+// we can call it with the super() keyword!
